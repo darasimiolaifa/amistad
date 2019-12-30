276f9be133c6f7dca683d88d1ca55294e0b3a8cd
Olaifa Oluwadarasimi Ibikunle <darasimiolaifa@gmail.com>
2019-12-30 13:46:12 +0100

    feat(profile): implement profile image upload
    - create image upload input type and button
    - write action to handle image upload from the UI

--- amistad-client/src/components/Profile.js
+++ amistad-client/src/components/Profile.js
@@ -1,15 +1,19 @@
 import React, { Fragment } from "react";
 import dayjs from "dayjs";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Paper from "@material-ui/core/Paper";
 import MuiLink from "@material-ui/core/Link";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
 import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarToday from "@material-ui/icons/CalendarToday";
+import { uploadProfileImage } from "../redux/actions/userActions";
 
 const styles = theme => ({
   paper: {
@@ -60,11 +64,24 @@ const styles = theme => ({
 });
 
 function Profile({ classes }) {
+  const dispatch = useDispatch();
   const {
     credentials: { handle, createdAt, imageUrl, bio, website, location },
     loading,
     authenticated
   } = useSelector(state => ({ ...state.user }));
+
+  const handleImageChange = ({ target: { files } }) => {
+    const [image] = files;
+    const formData = new FormData();
+    formData.append("image", image, image.name);
+    uploadProfileImage(dispatch, formData);
+  };
+
+  const handleEditPicture = () => {
+    const fileinput = document.getElementById("imageInput");
+    fileinput.click();
+  };
 
   return !loading ? (
     authenticated ? (
@@ -72,6 +89,17 @@ function Profile({ classes }) {
         <div className={classes.profile}>
           <div className="image-wrapper">
             <img src={imageUrl} alt="profile" className="profile-image" />
+            <input
+              type="file"
+              id="imageInput"
+              hidden="hidden"
+              onChange={handleImageChange}
+            />
+            <Tooltip title="Edit profile picture" placement="top">
+              <IconButton onClick={handleEditPicture} className="button">
+                <EditIcon color="primary" />
+              </IconButton>
+            </Tooltip>
           </div>
           <hr />
           <div className="profile-details">
@@ -139,7 +167,6 @@ function Profile({ classes }) {
   ) : (
     <p>Loading...</p>
   );
-  // return profileMarkup;
 }
 
 export default withStyles(styles)(Profile);
